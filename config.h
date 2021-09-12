38c0131285acd94e03180627324c7f06149c997a
Jeremy Andrews Zantua <jeremyzantua@gmail.com>
2021-09-12 23:07:37 +0800

    Updated font preference and removed alpha

--- config.h
+++ config.h
@@ -5,7 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Noto sans mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
+//static char *font = "Noto sans mono:pixelsize=12:antialias=true:autohint=true";
+//static char *font = "Adobe Source Code Pro:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -94,7 +96,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.95;
+float alpha = 1;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
